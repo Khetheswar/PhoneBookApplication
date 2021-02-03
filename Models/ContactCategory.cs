e394dc326364404adcf2a42a8383d228171fec76
Khetheswar <78416744+Khetheswar@users.noreply.github.com>
2021-02-03 21:02:50 +0530

    Add files via upload

---
+++ Models/ContactCategory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PhoneBookApplication.Models
+{
+    /// <summary>
+    /// Category class 
+    /// </summary>
+    public class ContactCategory
+    {
+        [Required]
+        public int CategoryId { get; set; }
+        [Required]
+        public string CategoryName { get; set; }
+    }
+}
